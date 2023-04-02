4020cfcd8606680fb6bccff02cee5cf902bc0dfd
ukioxz <ikatember@gmail.com>
2023-04-02 22:04:51 +0300

    add home page and items page

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("<h4>Home</h4>")
+    return render(request, 'main/index.html')
+
+def about(request):
+    return render(request, 'main/about.html')
